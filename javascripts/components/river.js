7ee5d5a4b1f85c13cf7563a54f46e2e3aba7d825
Beth <bethhnielsen56@gmail.com>
2020-03-01 21:40:51 -0600

    buttons made, not working yet

--- javascripts/components/river.js
+++ javascripts/components/river.js
@@ -9,9 +9,17 @@ const buildBearCards = () => {
     bearCards.forEach((bears) => {
     domString += '<div class="col-4 mb-2">'
     domString += '<div id="bear-card" class="card mb-4 h-100">';
-    domString += `<img src="${bears.image}" class="card-img-top h-75" alt="Picture of Bear seen">`;
-    domString += '<div class="card-body h-25">';
+    domString += `<img src="${bears.image}" class="card-img-top " alt="Picture of Bear seen">`;
+    domString += '<div class="card-body">';
     domString += `<h5 class="card-text text-center">${bears.name}</h5>`;
+    domString += `<div class="row" id="fish-buttons">`;
+    domString += `<div class="col-6" id="fish-attempt">`;
+    domString += '<button type="button" id="bear-form-submit" class="bear-form-button btn btn-block text-white">Attempt</button>';
+    domString += '</div>';
+    domString += `<div class="col-6" id="fish-caught">`;
+    domString += '<button type="button" id="bear-form-submit" class="bear-form-button btn btn-block text-white">Caught</button>';
+    domString += '</div>';
+    domString += '</div>';
     domString += '</div>';
     domString += '</div>';
     domString += '</div>';
